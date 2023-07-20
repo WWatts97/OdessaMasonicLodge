5bf9db96369add7f919233b44d6dbd482114e653
WWatts97 <awnewatts97@outlook.com>
2023-07-19 22:24:50 -0500

    deployed mobile friendly version 1.0

--- OdessaMasonFireworks/OdessaMasonFireworks.UI.MVC/wwwroot/js/scripts.js
+++ OdessaMasonFireworks/OdessaMasonFireworks.UI.MVC/wwwroot/js/scripts.js
@@ -1,7 +1,19 @@
-/*!
-* Start Bootstrap - Modern Business v5.0.7 (https://startbootstrap.com/template-overviews/modern-business)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+$(document).ready(function () {
+    if (window.innerWidth < 992) {
+        $('.smallhide').addClass('visually-hidden');
+    }
+});
+
+$(document).ready(function () {
+    if (window.innerWidth > 992) {
+        $('.smallhide').removeClass('visually-hidden');
+    }
+});
+
+$(window).resize(function () {
+    if (window.innerWidth < 992) {
+        $('.smallhide').addClass('visually-hidden');
+    } else {
+        $('.smallhide').removeClass('visually-hidden');
+    }
+})
